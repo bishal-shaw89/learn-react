1ab02d07f9f7203be1c61af5a6329068e11e5288
bishal-arodek <bishal.shaw@arodek.com>
2023-06-16 16:35:58 +0530

    component practice exercise completed

--- src/components/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-
-function Card(props) {
-  const classes = "card " + props.className;
-  //props.childern uses the data inside the custom component, in this example where the Card component is using the data between the custon tags(Card) will be passed here in props.children.
-  return <div className={classes}>{props.children}</div>;
-}
-
-export default Card;
---
+++ src/components/ConceptList.js
@@ -0,0 +1,11 @@
+function ConceptList(props) {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+export default ConceptList;
