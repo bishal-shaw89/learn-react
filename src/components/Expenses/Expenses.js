c11fc0af25d2c9616d6d7da6a34ef38322a00a7b
bishal-arodek <bishal.shaw@arodek.com>
2023-06-21 14:57:14 +0530

    demo project completed

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -20,6 +21,7 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         {/* {filteredExpenses.length === 0 && <p>No Expenses found.</p>} // when we use '&&' javascript will 
         validate the condition and if condition is true then it will execute the statement after '&&' this is 
         known as abusing*/}
